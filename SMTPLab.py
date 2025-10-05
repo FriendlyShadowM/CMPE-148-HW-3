5fc7aebbf1eb9a7560a75d0cb4a95ba1a69f200b
Devin Roberson <devinroberson01@gmail.com>
2025-10-05 15:45:55 -0700

    added some stuff

--- SMTPLab.py
+++ SMTPLab.py
@@ -4,11 +4,12 @@ msg = "\r\n I love computer networks!"
 endmsg = "\r\n.\r\n" 
 
 # Choose a mail server (e.g. Google mail server) and call it mailserver 
-mailserver = #Fill in start   #Fill in end 
+mailserver = ("")
 
 # Create socket called clientSocket and establish a TCP connection with mailserver 
 #Fill in start   
-
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(mailserver)
 #Fill in end 
 
 recv = clientSocket.recv(1024).decode() 
@@ -26,30 +27,47 @@ if recv1[:3] != '250':
 
 # Send MAIL FROM command and print server response. 
 # Fill in start 
-
+recv2 = clientSocket.recv(1024).decode() 
+print(recv2) 
+if recv2[:3] != '250': 
+    print('250 reply not received from server.') 
 # Fill in end 
 
 # Send RCPT TO command and print server response.  
 # Fill in start 
-
+recv3 = clientSocket.recv(1024).decode() 
+print(recv3) 
+if recv3[:3] != '250': 
+    print('250 reply not received from server.') 
 # Fill in end 
 
 # Send DATA command and print server response.  
 # Fill in start 
-
+recv4 = clientSocket.recv(1024).decode() 
+print(recv4) 
+if recv4[:3] != '250': 
+    print('250 reply not received from server.') 
 # Fill in end 
 
 # Send message data. 
 # Fill in start 
-
+clientSocket.send(msg.encode())
 # Fill in end 
 
 # Message ends with a single period. 
 # Fill in start 
-
+clientSocket.send(endmsg.encode())
+recv5 = clientSocket.recv(1024).decode() 
+print(recv5) 
+if recv5[:3] != '250': 
+    print('250 reply not received from server.') 
 # Fill in end 
 
 # Send QUIT command and get server response. 
 # Fill in start 
-
+recv6 = clientSocket.recv(1024).decode() 
+print(recv6) 
+if recv6[:3] != '250': 
+    print('250 reply not received from server.') 
 # Fill in end
+clientSocket.close()
